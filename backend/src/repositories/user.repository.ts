80728a6d8acf7f20c5da90203a00ca6ef6d67c1e
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-12 23:44:00 +0500

    User repository updated

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -9,16 +9,12 @@ class UserRepository {
   private tables: any;
 
   constructor() {
-
-    this.tables = {
-      sequelize: db.User,
-    };
+    this.tables = { sequelize: db.User };
   }
 
   buildIncludes(include: string[] = []) {
 
-    const associations =
-      db.User.associations || {};
+    const associations = db.User.associations || {};
 
     return [...new Set(include)]
       .filter((alias) => associations[alias])
@@ -28,11 +24,7 @@ class UserRepository {
   }
 
   async create(data: any) {
-
-    return dbHelper.create(
-        this.tables,
-        data
-    );
+    return dbHelper.create(this.tables, data);
   }
 
   async findAll(filters: any = {}, options: any = {}) {
@@ -40,18 +32,15 @@ class UserRepository {
     const where: any = {};
 
     if (filters.business_code) {
-      where.business_code =
-          filters.business_code;
+      where.business_code = filters.business_code;
     }
 
     if (filters.user_type) {
-      where.user_type =
-          filters.user_type;
+      where.user_type = filters.user_type;
     }
 
     if (filters.is_active) {
-      where.is_active =
-          filters.is_active;
+      where.is_active = filters.is_active;
     }
 
     if (filters.search) {
@@ -61,20 +50,17 @@ class UserRepository {
       where[Op.or] = [
         {
           name: {
-            [Op.like]:
-                `%${filters.search}%`
+            [Op.like]: `%${filters.search}%`
           }
         },
         {
           email: {
-            [Op.like]:
-                `%${filters.search}%`
+            [Op.like]: `%${filters.search}%`
           }
         },
         {
           phone: {
-            [Op.like]:
-                `%${filters.search}%`
+            [Op.like]: `%${filters.search}%`
           }
         },
       ];
@@ -84,15 +70,9 @@ class UserRepository {
         this.tables,
         {
           where,
-
-          include: this.buildIncludes(
-              options.include || []
-          ),
-
+          include: this.buildIncludes(options.include || []),
           limit: options.limit,
-
           offset: options.offset,
-
           order: options.order || [
             ["created_at", "DESC"]
           ],
@@ -100,10 +80,7 @@ class UserRepository {
     );
   }
 
-  async findByCode(
-      userCode: string,
-      options: any = {}
-  ) {
+  async findByCode(userCode: string, options: any = {}) {
 
     return dbHelper.findOne(
         this.tables,
@@ -131,10 +108,7 @@ class UserRepository {
     );
   }
 
-  async update(
-      userCode: string,
-      data: any
-  ) {
+  async update(userCode: string, data: any) {
 
     return dbHelper.update(
         this.tables,
