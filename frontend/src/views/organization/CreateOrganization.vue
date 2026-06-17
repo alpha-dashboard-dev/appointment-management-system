dba98464998261efbb88802ae282cea61b8f28b0
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-17 21:26:24 +0500

    Add API Handler in Create Organization

--- frontend/src/views/organization/CreateOrganization.vue
+++ frontend/src/views/organization/CreateOrganization.vue
@@ -33,8 +33,8 @@
 <script setup>
 import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import api from '@/utils/api'
 import { validateOrganizationForm } from '@/utils/validator'
+import {apiHandler} from "../../utils/api/apiHandler.js";
 
 const router = useRouter()
 
@@ -57,10 +57,11 @@ async function submit() {
   loading.value = true
   error.value = ''
   try {
-    await api.post('/organizations/create-organization', form)
+    const res = await apiHandler("organization", "createOrganization", form)
+    // console.log(res.message)
     router.push('/organizations')
   } catch (err) {
-    error.value = err.response?.data?.message || 'Failed to create organization'
+    error.value = err.message || 'Failed to create organization'
   } finally {
     loading.value = false
   }
