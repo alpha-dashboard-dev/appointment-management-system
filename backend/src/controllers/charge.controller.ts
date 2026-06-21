a6ed89a8012b9c46b4635b68aee6d8b4a3215ada
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-21 17:38:06 +0500

    Add delete and getbyAnyField method

--- backend/src/controllers/charge.controller.ts
+++ backend/src/controllers/charge.controller.ts
@@ -14,7 +14,19 @@ class ChargeController {
 
     async getAll(req: Request, res: Response) {
         try {
-            const data = await service.getAll(req.query, req.user);
+            let include = req.query.include ?? "";
+            include = [
+                {
+                    alias: "business",
+                    attributes: [],
+                },
+            ]
+            const data = await service.getAll(
+                {
+                    ...req.query,
+                    include
+                },
+                req.user);
             return res.status(200).json({ success: true, data });
         } catch (err: any) {
             return res.status(500).json({ success: false, message: err.message });
@@ -23,10 +35,11 @@ class ChargeController {
 
     async getByCode(req: Request, res: Response) {
         try {
+            const chargeCode = String(req.params.chargeCode)
             const data = await service.getByCode(
-                String(req.params.chargeCode),
+                chargeCode,
                 req.user,
-                req.query
+                // req.query
             );
             return res.status(200).json({ success: true, data });
         } catch (err: any) {
@@ -34,9 +47,39 @@ class ChargeController {
         }
     }
 
+    async getByAnyField(req: Request, res: Response) {
+    
+            try {
+                // console.log(req.query)
+                const { include, ...where } = req.query;
+                // console.log(field, value)
+                const user = req.user;
+                const data = await service.getOne(
+                    where,
+                    user,
+                    {
+                        include
+                    }
+                )
+    
+                return res.status(200).json({
+                    success: true,
+                    data,
+                });
+    
+            } catch (err: any) {
+    
+                return res.status(404).json({
+                    success: false,
+                    message: err.message,
+                });
+            }
+        }
+
     async update(req: Request, res: Response) {
         try {
-            const data = await service.update(String(req.params.chargeCode), req.body, req.user);
+            const chargeCode = String(req.params.chargeCode)
+            const data = await service.update(chargeCode, req.body, req.user);
             return res.status(200).json({ success: true, message: "Charge updated", data });
         } catch (err: any) {
             return res.status(400).json({ success: false, message: err.message });
@@ -46,11 +89,13 @@ class ChargeController {
     async deactivate(req: Request, res: Response) {
 
         try {
-
-            const data = await service.deactivate(
-                    String(req.params.chargeCode),
-                    req.body.status
-                );
+            const chargeCode = String(req.params.chargeCode)
+            const data = req.body
+            await service.deactivate(
+                chargeCode,
+                data,
+                req.user
+            );
 
             return res.status(200).json({
                 success: true,
